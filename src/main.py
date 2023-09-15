9f692e633004a6cec2fd3b677f87a26605c3914d
colin2gg <colin.key2@outlook.com>
2023-09-15 09:29:26 -0700

    baseline image class
    added tagging support to image class
    
    return tags
    set tags
    add tag (at start?)

--- src/main.py
+++ src/main.py
@@ -24,10 +24,87 @@ class GUI(QtWidgets.QMainWindow):
 
         self.setWindowTitle('Image Caption')
 
+        self.menuDict = self.buildTopBar()
+        
+    def buildTopBar(self):
+        menu = self.menuBar()
+        fileMenu = menu.addMenu("File")
+        openFolder = fileMenu.addAction("Open Folder")
+        return({
+             "Menu":menu,
+             "FileMenu":fileMenu,
+             "openFolder":openFolder
+                })
 
+class tagger():
+     
+     def __init__(self):
+          pass
+
+class image():
+     
+    def __init__(self, path):
+        
+
+        if os.path.exists(path) == False:
+            raise Exception("image file does not exist")
+        
+        self.imagePath = path
+        self.tagFile = "{}.txt".format(os.path.splitext(self.imagePath)[0])
+        self.hasTagFolder = os.path.exists(self.tagFile)
+        print (self.imagePath)
+        print (self.tagFile)
+        print (self.hasTagFolder)
+    
+    def createTagFile(self):
+        with open(self.tagFile, 'w') as fp:
+            pass
+    
+    def returnTags(self):
+        tags = ""
+        if os.path.exists(self.tagFile):
+            with open(self.tagFile, 'r') as tf:
+                tags = tf.read()
+        return(tags.split(', '))
+    
+    def addTag(self, tag = "", insertAtStart = False):
+        if not tag:
+            return()
+
+        newtags = tag.split(', ')
+        currentTags = self.returnTags()
+        
+        if not currentTags[0] == '':
+            if insertAtStart:
+                newtags = newtags + currentTags
+            else:
+                newtags = currentTags + newtags
+
+        seperator = ', '
+        tagsAsString = seperator.join(newtags)
+
+        textFile = open(self.tagFile, "w")
+        textFile.write(tagsAsString)
+    
+    def setTags(self, tag = []):
+        seperator = ', '
+        tag = seperator.join(tag)
+        textFile = open(self.tagFile, 'w')
+        textFile.write(tag)
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
+    test = image("/Users/grettaklu/Desktop/profileEmi--1.jpg")
+    test.createTagFile()
+    test.addTag("test")
+    test.addTag("after test")
+    test.addTag("before test", True)
+    print (test.returnTags())
+    test.setTags(['new before', 'new test', 'new after'])
+    print (test.returnTags())
+    
+    '''
+    app = QApplication(sys.argv)
 	gui = GUI()
 	gui.show()
 	sys.exit(app.exec())
+    '''
